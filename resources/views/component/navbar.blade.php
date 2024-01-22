c556bd224595244954fbb7eb7dabf4ea36d7f21d
RushaidKhan <rushaidk7@gmail.com>
2024-01-22 08:14:00 +0530

    basic ui

---
+++ resources/views/component/navbar.blade.php
@@ -0,0 +1,10 @@
+<div>
+    <nav>
+        <a href="#">Home</a>
+        <a href="#">Store</a>
+        <a href="#">About-us</a>
+        <a href="#">Blog</a>
+        <a href="#">Login</a>
+        <a href="#">Register</a>
+    </nav>
+</div>
