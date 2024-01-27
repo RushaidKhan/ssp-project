772e67fdb25080a7cab55c670de05fcdde75321b
RushaidKhan <rushaidk7@gmail.com>
2024-01-27 14:13:40 +0530

    minor changes in store page

--- resources/views/store.blade.php
+++ resources/views/store.blade.php
@@ -2,7 +2,16 @@
 
 @section('content')
     @include('components.navbar')
-    <div>
-        
+    <div class="storeWrapper">
+        <section class="storeHeader">
+            <div>
+            </div>
+            <img src="{{ asset('media/store.jpg') }}">
+            <h1 class="centered">Explore our distinguished selection of opulent wedding venues and gourmet catering services.</h1>
+        </section>
+        <br>
+        <section class="productsHolder">
+
+        </section>
     </div>
 @endsection
