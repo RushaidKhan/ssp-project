3b01302924b699dd5e620d409bb13163e6edcf38
RushaidKhan <rushaidk7@gmail.com>
2024-02-14 20:17:59 +0530

    designed requestForm

--- resources/views/Request/requestForm.blade.php
+++ resources/views/Request/requestForm.blade.php
@@ -1,11 +1,14 @@
 @extends('layout')
 @section('content')
     @include('components.navbar')
-    <form action="{{ route('venues.submit-request', $venue) }}" method="post">
-        @csrf
-        <label for="requested_date">Choose a Date:</label>
-        <input type="date" id="requested_date" name="requested_date" required>
-        <button type="submit">Submit Request</button>
-    </form>
+    <br>
+    <div class="requestFormWrapper">
+        <form action="{{ route('venues.submit-request', $venue) }}" method="post">
+            @csrf
+            <label for="requested_date">Choose a Date:</label><br>
+            <input type="date" id="requested_date" name="requested_date" required><br>
+            <button type="submit">Make a Request</button>
+        </form>
+    </div>
 @endsection
 
